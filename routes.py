69e7de75fc6ade923b2d64bc9c47890377621706
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-11-11 00:40:21 +0800

    added requirements.txt

--- routes.py
+++ routes.py
@@ -1,2 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from pymongo import MongoClient
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return 'Hello, World!'
+
+app.register_blueprint(main)
+
+if __name__ == '__main__':
+    app.run(debug=True)
