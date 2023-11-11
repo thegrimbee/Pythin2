b377b5c0fbceca8badba6b18e55445f67ff397f7
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-11-11 12:39:28 +0800

    Update __init__.py

--- Pythin2/__init__.py
+++ Pythin2/__init__.py
@@ -1,11 +1,19 @@
 
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+#Load .env file
+dotenv_path = dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
+load_dotenv(dotenv_path)
+
+# Create the application
 def create_app():
     app = Flask(__name__)
 
     # Add any necessary configuration here
-
+    app.config['SECRET_KEY'] = os.getenv('PYTHIN2_SECRET_KEY')
+    
     # Add any necessary routes here
 
     return app
