4669543f17e290125b7c009df03cf454fdb26f96
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-11-12 15:08:37 +0800

    Update stuff

--- Pythin2/main/views.py
+++ Pythin2/main/views.py
@@ -1,13 +1,12 @@
-
 from flask import Blueprint, render_template
 from . import main_bp
 
 @main_bp.route('/')
 def index():
     print("Index page being rendered...")
-
     return render_template('index.html')
 
 @main_bp.route('/about')
 def about():
+    print("About page being rendered...")
     return render_template('about.html')
--- Pythin2/user/views.py
+++ Pythin2/user/views.py
@@ -1,12 +1,13 @@
-
 from flask import Blueprint, render_template
+import pymongo
+from . import user_bp
 
-main_bp = Blueprint('main_bp', __name__)
+@user_bp.route('/login')
+def login():
+    print("Login page being rendered...")
+    return render_template('login.html')
 
-@main_bp.route('/')
-def index():
-    return render_template('index.html')
-
-@main_bp.route('/about')
-def about():
-    return render_template('about.html')
+@user_bp.route('/register')
+def register():
+    print("Register page being rendered...")
+    return render_template('register.html')
