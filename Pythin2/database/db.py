d129769b93c22fe14b555db76ab93f4e69aaee49
thegrimbee <gabriel.mario.antaputra@gmail.com>
2023-11-12 16:30:40 +0800

    Update a lot of stuff

---
+++ Pythin2/database/db.py
@@ -0,0 +1,43 @@
+
+from pymongo import MongoClient
+import hashlib
+
+class Database:
+    def __init__(self, uri):
+        self.client = MongoClient(uri)
+        self.db = self.client.get_database()
+
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+
+class UserDatabase(Database):
+    def __init__(self):
+        print("UserDatabase initializing...")
+        self.__init__('mongodb://localhost:27017')
+        self.users = self.get_collection("users")
+    
+    def user_exists(self, username):
+        print("Checking if user exists...")
+        return self.users.find_one({"username": username}) != None
+    
+    def create_user(self, username, password):
+        print("Creating user...")
+        if self.user_exists(username):
+            print("User already exists")
+            return False
+        else:
+            print("User added to database")
+            hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            self.users.insert_one({"username": username, "password": hashed_password})
+            return True
+
+    def login_user(self, username, password):
+        print("Logging in user...")
+        if self.user_exists(username):
+            hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            return self.users.find_one({"username": username, "password": hashed_password}) != None
+        else:
+            print("User does not exist")
+            return False
+
+user_db = UserDatabase()
--- Pythin2/user/views.py
+++ Pythin2/user/views.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, render_template
-import pymongo
+from flask import Blueprint, render_template, request, redirect, url_for
+from ..database.db import user_db
 from . import user_bp
 
-@user_bp.route('/login')
+@user_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    print("Login page being rendered...")
-    return render_template('login.html')
+    if request.method == 'POST':
+        print("Login page being rendered...")
+        username = request.form['username']
+        password = request.form['password']
+        if user_db.login_user(username, password):
+            print(f"{username} logged in")
+            return redirect(url_for('index'))
+        else:
+            print("Login failed")
+            return render_template('login.html')
+    else :
+        print("Login page being rendered...")
+        return render_template('login.html')
 
 @user_bp.route('/register')
 def register():
